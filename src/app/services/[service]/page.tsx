4692d0e96f07a5cf5d7c592f4b6b57606704961a
Babina081 <babitamoo333@gmail.com>
2024-09-06 15:38:08 +0545

    fix deploy error

--- src/app/services/[service]/page.tsx
+++ src/app/services/[service]/page.tsx
@@ -1,15 +1,13 @@
 "use client";
 import Heading from "@/components/Heading";
 import { useParams } from "next/navigation";
-import { useRouter } from "next/router";
 import React from "react";
 
 const ServicePage = () => {
-  // Use useParams to get the dynamic parameter
   const { service } = useParams();
   return (
     <div className="text-3xl font-bold">
-      <Heading>{service}</Heading> {/* Clean up service name */}
+      <Heading>{service}</Heading> 
     </div>
   );
 };
