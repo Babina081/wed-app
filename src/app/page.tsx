d0e69c0549223c46958eb96c3603e2dff9b54056
Babina081 <babitamoo333@gmail.com>
2024-09-06 15:30:37 +0545

    add component in root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Banner from "@/components/Banner";
 import Categories from "@/components/Categories";
+import ContactBanner from "@/components/ContactBanner";
+import Spotlight from "@/components/Spotlight";
 
 export default function Home() {
   return (
     <>
       <Banner></Banner>
       <Categories></Categories>
+      <Spotlight></Spotlight>
+      <ContactBanner></ContactBanner>
     </>
   );
 }
