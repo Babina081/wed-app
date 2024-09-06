0ea14e34ad36937c9a5c31c88b2d2fb5233effbf
Babina081 <babitamoo333@gmail.com>
2024-09-06 14:48:22 +0545

    add Banner component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Heading from "./Heading";
+
+const Banner = () => {
+  return (
+    <div
+      className="bg-purple-200 h-[60vh] py-4
+   px-10 flex flex-col items-center justify-center text-center"
+    >
+      <Heading> Lets find a team for your events and celebrations</Heading>
+      <p className="text-xl text-gray-600">
+        Discover the best photographers, videographers, boutiques, make-up
+        artists, event planners and more
+      </p>
+      {/* input search */}
+    </div>
+  );
+};
+
+export default Banner;
