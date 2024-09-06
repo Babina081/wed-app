1be024a4c8f907200338421b64b3ada304189349
Babina081 <babitamoo333@gmail.com>
2024-09-06 14:48:15 +0545

    add catgeories component

---
+++ src/components/Categories.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Heading from "./Heading";
+import { eventServices } from "@/utils/data";
+import Image from "next/image";
+import Link from "next/link";
+
+const Categories = () => {
+  return (
+    <div
+      className=" py-4
+   px-10 flex items-center justify-center flex-col"
+    >
+      <Heading>Categories</Heading>
+      <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 w-full mt-4">
+        {eventServices.map((category) => (
+          <Link
+            href={`/services/${category.name.toLowerCase().replace(/ /g, "-")}`}
+            key={category.name}
+            className="border p-3 rounded-lg gap-2 flex items-center flex-col hover:bg-slate-500/15  transition-all duration-300 ease-in-out cursor-pointer"
+          >
+            <img
+              src={category.imageUrl}
+              alt={category.name}
+              className="w-full h-full   object-cover rounded-lg"
+            />
+            <span>{category.name}</span>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
