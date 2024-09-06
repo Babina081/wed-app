850c66f6e0b414d1b89b8dc401df2f287611888d
Babina081 <babitamoo333@gmail.com>
2024-09-06 14:47:48 +0545

    create data file

---
+++ src/utils/data.js
@@ -0,0 +1,38 @@
+export const eventServices = [
+  {
+    name: "Photographers",
+    imageUrl: "https://picsum.photos/200/200?random=1",
+  },
+  {
+    name: "Videographers",
+    imageUrl: "https://picsum.photos/200/200?random=2",
+  },
+  {
+    name: "Hair and Beauty",
+    imageUrl: "https://picsum.photos/200/200?random=3",
+  },
+  {
+    name: "Boutiques",
+    imageUrl: "https://picsum.photos/200/200?random=4",
+  },
+  {
+    name: "Florists",
+    imageUrl: "https://picsum.photos/200/200?random=5",
+  },
+  {
+    name: "Cakes and Bakeries",
+    imageUrl: "https://picsum.photos/200/200?random=6",
+  },
+  {
+    name: "Venues",
+    imageUrl: "https://picsum.photos/200/200?random=7",
+  },
+  {
+    name: "Event Planners",
+    imageUrl: "https://picsum.photos/200/200?random=8",
+  },
+  {
+    name: "Travel Experts",
+    imageUrl: "https://picsum.photos/200/200?random=9",
+  },
+];
