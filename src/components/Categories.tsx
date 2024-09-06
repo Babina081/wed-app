777b9555d9db5f07cc317a83318c160400d14f8d
Babina081 <babitamoo333@gmail.com>
2024-09-06 20:04:17 +0545

    add loading state

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Heading from "./Heading";
 import { eventServices } from "@/utils/data";
 import Link from "next/link";
+import LoadingSpinner from "./LoadingSpinner";
 
 const Categories = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+  
   return (
     <section
       className=" py-10
@@ -17,12 +25,18 @@ const Categories = () => {
             key={category.name}
             className="border p-3 rounded-lg gap-2 flex items-center flex-col hover:bg-slate-500/15  transition-all duration-300 ease-in-out cursor-pointer"
           >
-            <img
-              src={category.imageUrl}
-              alt={category.name}
-              className="w-full h-full   object-cover rounded-lg"
-            />
-            <span>{category.name}</span>
+            {loading ? (
+              <LoadingSpinner />
+            ) : (
+              <>
+                <img
+                  src={category.imageUrl}
+                  alt={category.name}
+                  className="w-full h-full object-cover rounded-lg"
+                />
+                <span>{category.name}</span>
+              </>
+            )}
           </Link>
         ))}
       </div>
--- src/components/Spotlight.tsx
+++ src/components/Spotlight.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Heading from "./Heading";
 import { spotlightServices } from "@/utils/data";
 import Link from "next/link";
+import LoadingSpinner from "./LoadingSpinner";
 
 const Spotlight = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+  
   return (
     <section
       className="py-20
@@ -19,30 +27,36 @@ const Spotlight = () => {
             key={spotlight.name}
             className="border p-3 rounded-lg gap-2 flex items-center  flex-col hover:bg-slate-500/15  transition-all duration-300 ease-in-out cursor-pointer "
           >
-            <div className="relative mb-4">
-              <img
-                src={spotlight.image}
-                alt={spotlight.name}
-                className="w-full h-full   object-cover rounded-lg"
-              />
-              <div className="h-10 w-10 rounded-full p-2 bg-white border-2  border-black flex items-center justify-center absolute right-5 -bottom-5 z-0">
-                {spotlight.rank}
-              </div>
-            </div>
-            <Heading>{spotlight.name}</Heading>
-            <p className="text-sm text-red-600">{spotlight.category}</p>
-            <p className="text-sm text-gray-600">{spotlight.location}</p>
-            <ul className="w-full grid  grid-cols-2 gap-2">
-              {spotlight.tags.map((tag) => (
-                <li
-                  key={tag.label}
-                  className="flex flex-col items-center justify-start border rounded-lg p-2 text-center gap-1"
-                >
-                  <tag.icon className="inline-block mr-2 h-5 w-5" />
-                  <div className="text-xs">{tag.label}</div>
-                </li>
-              ))}
-            </ul>
+            {loading ? (
+              <LoadingSpinner />
+            ) : (
+              <>
+                <div className="relative mb-4">
+                  <img
+                    src={spotlight.image}
+                    alt={spotlight.name}
+                    className="w-full h-full   object-cover rounded-lg"
+                  />
+                  <div className="h-10 w-10 rounded-full p-2 bg-white border-2  border-black flex items-center justify-center absolute right-5 -bottom-5 z-0">
+                    {spotlight.rank}
+                  </div>
+                </div>
+                <Heading>{spotlight.name}</Heading>
+                <p className="text-sm text-red-600">{spotlight.category}</p>
+                <p className="text-sm text-gray-600">{spotlight.location}</p>
+                <ul className="w-full grid  grid-cols-2 gap-2">
+                  {spotlight.tags.map((tag) => (
+                    <li
+                      key={tag.label}
+                      className="flex flex-col items-center justify-start border rounded-lg p-2 text-center gap-1"
+                    >
+                      <tag.icon className="inline-block mr-2 h-5 w-5" />
+                      <div className="text-xs">{tag.label}</div>
+                    </li>
+                  ))}
+                </ul>
+              </>
+            )}
           </Link>
         ))}
       </div>
