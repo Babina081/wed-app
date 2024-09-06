cded83baef39e4fa31200332db4ade7203cae3a2
Babina081 <babitamoo333@gmail.com>
2024-09-06 20:51:04 +0545

    update loader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const loading = () => {
   return (
     <div
-      className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-gray-700 border-t-transparent bg-white"
+      className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-gray-700 border-t-transparent bg-white fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
       role="status"
     >
       <span className="absolute -m-px h-px w-px overflow-hidden whitespace-nowrap border-0 p-0 [clip:rect(0,0,0,0)]">
