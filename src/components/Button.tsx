b7655a33620b06fa0bcaae74dbe2879aaf5073a4
Babina081 <babitamoo333@gmail.com>
2024-09-06 16:05:12 +0545

    update button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ children }: { children: React.ReactNode }) => {
   return (
-    <button className="bg-red-400 text-white font-bold hover:bg-red-100 p-2 rounded-lg transition-all duration-300 ease-in-out">
+    <button className="bg-red-400 text-white font-bold hover:bg-red-100 py-2 px-10 rounded-lg transition-all duration-300 ease-in-out ">
       {children}
     </button>
   );
