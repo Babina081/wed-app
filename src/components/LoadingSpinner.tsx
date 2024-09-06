193e61b1aee4351f0ac6fcf6824f8780fce2b969
Babina081 <babitamoo333@gmail.com>
2024-09-06 20:03:51 +0545

    add loader

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LoadingSpinner = () => {
+  return (
+    <div
+      className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-gray-700 border-t-transparent bg-white"
+      role="status"
+    >
+      <span className="absolute -m-px h-px w-px overflow-hidden whitespace-nowrap border-0 p-0 [clip:rect(0,0,0,0)]">
+        Loading...
+      </span>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
