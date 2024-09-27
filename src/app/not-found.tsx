38deb4a3a09109787245e373b15609622a19254e
Babina081 <babitamoo333@gmail.com>
2024-09-27 13:22:38 +0545

    add not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,13 @@
 "use client";
+import Image from "next/image";
 import React from "react";
-
+import emptyImage from "../assets/not-found.svg";
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Image src={emptyImage} alt="not found" className="size-64"></Image>
+    </div>
+  );
 };
 
 export default NotFound;
